764cd133ea864bcae1cbd4a440ac94570d1c307f
rubicon2 <41235563+rubicon2@users.noreply.github.com>
2024-10-28 10:26:45 +0000

    Add 404 and error views

--- app.mjs
+++ app.mjs
@@ -4,6 +4,7 @@ import 'dotenv/config';
 const PORT = process.env.PORT || 8000;
 
 const app = express();
+app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
   res.send('Hello, world!');
@@ -11,13 +12,13 @@ app.get('/', (req, res) => {
 
 // If no prior routes have been matched, show 404.
 app.use((req, res) => {
-  res.status(404).send('404: page not found.');
+  res.status(404).render('404', { title: '404 - page not found' });
 });
 
 // Error handling.
 app.use((error, req, res, next) => {
   console.error(error);
-  res.status(500).send('An internal server error has occurred.');
+  res.status(500).render('error', { title: 'Error', error });
 });
 
 app.listen(PORT, () => console.log('Server listening on port', PORT));
