3fb31b6d9e221a115bcef1578f3636ce6b789d85
rubicon2 <41235563+rubicon2@users.noreply.github.com>
2024-11-22 10:42:21 +0000

    Prevent a user from adding or editing manufacturers if they are not logged in

--- controllers/manufacturerController.mjs
+++ controllers/manufacturerController.mjs
@@ -23,69 +23,97 @@ async function getManufacturer(req, res) {
   });
 }
 
-function getNewManufacturerForm(req, res) {
-  res.render('manufacturers/newManufacturer', { title: 'New manufacturer' });
-}
+function getNewManufacturerForm(req, res, next) {
+  try {
+    const { isLoggedIn } = req.session;
+    if (!isLoggedIn) throw new Error('You are not logged in.');
 
-async function postNewManufacturerForm(req, res) {
-  const { name, description } = req.body;
-
-  let buffer = null;
-  let mimetype = null;
-  if (req.file) {
-    buffer = req.file.buffer.toString('base64');
-    mimetype = req.file.mimetype;
+    res.render('manufacturers/newManufacturer', { title: 'New manufacturer' });
+  } catch (error) {
+    next(error);
   }
-
-  const { id } = await db.addManufacturer({
-    name,
-    description,
-    img_data: buffer,
-    img_type: mimetype,
-  });
-  res.status(303).redirect(`/manufacturer/${id}`);
 }
 
-async function getEditManufacturerForm(req, res) {
-  const [manufacturer, manufacturers] = await Promise.all([
-    db.getManufacturer(req.params.id),
-    db.getAllManufacturers(),
-  ]);
-  res.render(`manufacturers/editManufacturer`, {
-    title: 'Edit manufacturer',
-    manufacturer,
-    manufacturers,
-    isLoggedIn: req.session.isLoggedIn,
-  });
-}
+async function postNewManufacturerForm(req, res, next) {
+  try {
+    const { isLoggedIn } = req.session;
+    if (!isLoggedIn) throw new Error('You are not logged in.');
 
-async function postEditManufacturerForm(req, res) {
-  const id = req.params.id;
+    const { name, description } = req.body;
 
-  let buffer = null;
-  let mimetype = null;
-  let { img_data: previous_img_data, img_type: previous_img_type } =
-    await db.getManufacturer(id);
-  if (req.file) {
-    // If a file was added in the form, use it.
-    buffer = req.file.buffer.toString('base64');
-    mimetype = req.file.mimetype;
-  } else {
-    // If no new file in the form, just re-use old one.
-    buffer = previous_img_data;
-    mimetype = previous_img_type;
+    let buffer = null;
+    let mimetype = null;
+    if (req.file) {
+      buffer = req.file.buffer.toString('base64');
+      mimetype = req.file.mimetype;
+    }
+
+    const { id } = await db.addManufacturer({
+      name,
+      description,
+      img_data: buffer,
+      img_type: mimetype,
+    });
+    res.status(303).redirect(`/manufacturer/${id}`);
+  } catch (error) {
+    next(error);
   }
+}
 
-  // Update with the new details.
-  const { name, description } = req.body;
-  await db.updateManufacturer(id, {
-    id,
-    name,
-    description,
-    img_data: buffer,
-    img_type: mimetype,
-  });
-  res.status(303).redirect(`/manufacturer/${id}`);
+async function getEditManufacturerForm(req, res, next) {
+  try {
+    const { isLoggedIn } = req.session;
+    if (!isLoggedIn) throw new Error('You are not logged in.');
+
+    const [manufacturer, manufacturers] = await Promise.all([
+      db.getManufacturer(req.params.id),
+      db.getAllManufacturers(),
+    ]);
+    res.render(`manufacturers/editManufacturer`, {
+      title: 'Edit manufacturer',
+      manufacturer,
+      manufacturers,
+      isLoggedIn,
+    });
+  } catch (error) {
+    next(error);
+  }
+}
+
+async function postEditManufacturerForm(req, res, next) {
+  try {
+    const { isLoggedIn } = req.session;
+    if (!isLoggedIn) throw new Error('You are not logged in.');
+
+    const id = req.params.id;
+
+    let buffer = null;
+    let mimetype = null;
+    let { img_data: previous_img_data, img_type: previous_img_type } =
+      await db.getManufacturer(id);
+    if (req.file) {
+      // If a file was added in the form, use it.
+      buffer = req.file.buffer.toString('base64');
+      mimetype = req.file.mimetype;
+    } else {
+      // If no new file in the form, just re-use old one.
+      buffer = previous_img_data;
+      mimetype = previous_img_type;
+    }
+
+    // Update with the new details.
+    const { name, description } = req.body;
+    await db.updateManufacturer(id, {
+      id,
+      name,
+      description,
+      img_data: buffer,
+      img_type: mimetype,
+    });
+    res.status(303).redirect(`/manufacturer/${id}`);
+  } catch (error) {
+    next(error);
+  }
 }
 
 export {
